89aaf38a0789a5ab77299d52d8f5cad0cce61220
Melissa Sheehan <mxsheehan7@gmail.com>
2017-12-19 23:18:02 -0800

    day 1 and day 2

---
+++ day2.rb
@@ -0,0 +1,22 @@
+# process every line in a text file with ruby
+file='day1input.rb'
+
+  sum = 0
+  # for each line, calculate difference between max and min
+  File.readlines(file).each do |line|
+    lineArr = line.split(" ")
+    int_arr = Array.new
+    lineArr.each do |li|
+     int_arr << li.to_i #convert current index element to an int and put in int_arr
+    end
+
+    max = int_arr.max
+    # puts "max = #{max}"
+    min = int_arr.min
+    # puts "min = #{min}"
+    diff = max - min
+    # puts diff
+    sum = sum + diff
+  end
+
+  puts sum 
---
+++ day3.rb
@@ -0,0 +1,20 @@
+count_steps(num)
+
+  if( num == 1)
+    steps = 0
+  end
+
+  # move up 
+  if( num < )
+    steps = 0
+  end
+  # move down
+
+  # move left 
+
+  # move right 
+
+
+  
+
+end
